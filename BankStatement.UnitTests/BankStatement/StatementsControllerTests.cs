4f36124d38661539a82dd13a41ef82fe4eff8385
Ademir <ademir.belini@scalait.com>
2024-09-07 23:07:39 -0300

    Adding unit tests

---
+++ BankStatement.UnitTests/BankStatement/StatementsControllerTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using BankStatementApp.Controllers;
+using BankStatementApp.Interfaces;
+using BankStatementApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BankStatement.UnitTests.BankStatement
+{
+    public class StatementsControllerTests
+    {
+        private readonly Mock<ITransactionService> _serviceMock;
+        private readonly Mock<ILogger<StatementController>> _loggerMock;
+        private readonly StatementController _controller;
+
+        public StatementsControllerTests()
+        {
+            _loggerMock = new Mock<ILogger<StatementController>>();
+            _serviceMock = new Mock<ITransactionService>();
+            _controller = new StatementController(_loggerMock.Object, _serviceMock.Object);
+        }
+
+        [Fact]
+        public void GetAllTransactions_ShouldReturnOkResult()
+        {
+            // Arrange
+            var transactions = new List<BankTransaction>
+            {
+                new BankTransaction { Date = DateTime.Now, TransactionType = "Depósito", Amount = 1000 }
+            };
+
+            _serviceMock.Setup(service => service.GetAll()).Returns(transactions);
+
+            // Act
+            var result = _controller.GetStatement(20);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            
+            var anonymousObject = okResult.Value.GetType().GetProperty("transactions").GetValue(okResult.Value, null);
+            var returnedTransactions = Assert.IsAssignableFrom<List<BankTransaction>>(anonymousObject);
+
+            // Assert
+            Assert.Single(returnedTransactions);
+        }
+
+        [Fact]
+        public void GetAllTransactions_ShouldReturnNotFound_WhenNoTransactions()
+        {
+            // Arrange
+            _serviceMock.Setup(service => service.GetAll()).Returns(new List<BankTransaction>());
+
+            // Act
+            var result = _controller.GetStatement(20);
+
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+
+            var messageProperty = notFoundResult.Value.GetType().GetProperty("message");
+            var message = messageProperty?.GetValue(notFoundResult.Value, null)?.ToString();
+
+            // Assert
+            Assert.Equal("Nenhuma transação encontrada.", message);
+        }
+    }
+}
---
+++ BankStatement.UnitTests/BankStatement/TransactionServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankStatementApp.Interfaces;
+using BankStatementApp.Models;
+using BankStatementApp.Services;
+using Moq;
+
+namespace BankStatement.UnitTests.BankStatement
+{
+    public class TransactionServiceTests
+    {
+        private readonly Mock<ITransactionRepository> _repositoryMock;
+        private readonly TransactionService _transactionService;
+        public TransactionServiceTests()
+        {
+            _repositoryMock = new Mock<ITransactionRepository>();
+            _transactionService = new TransactionService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public void GetBankTransactions_ShouldReturnAllTransactions()
+        {
+            // Arrange
+            var transactions = new List<BankTransaction>
+            {
+                new BankTransaction { Date = DateTime.Now, TransactionType = "Depósito", Amount = 1000 },
+                new BankTransaction { Date = DateTime.Now.AddDays(-1), TransactionType = "Saque", Amount = -500 }
+            };
+
+            _repositoryMock.Setup(repo => repo.GetBankTransactions()).Returns(transactions);
+
+            // Act
+            var result = _transactionService.GetAll();
+
+            // Assert
+            Assert.Equal(2, result.Count());
+            Assert.Equal(1000, result.First().Amount);
+        }
+    }
+}
